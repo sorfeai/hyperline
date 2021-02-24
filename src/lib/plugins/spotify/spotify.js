e66f3c70645a9294b2ba118147aa3844ba54786a
Nikita Belousov <30675037+sorfeai@users.noreply.github.com>
2021-02-24 03:31:31 +0300

    implement timings and floating text for spotify

--- src/lib/plugins/spotify/spotify.js
+++ src/lib/plugins/spotify/spotify.js
@@ -8,37 +8,7 @@ import SvgIcon from '../../utils/svg-icon'
 
 const exec = util.promisify(cp.exec)
 
-const API_TOKEN = 'BQDzt4-NHlBc_blSFUuiLnnwGfMXdMZGz9RFx9PJbJqeJBD57F-JOaGGl2Z9lJPnmcyXMx37COlGNUnjburBM1CORJP8BaGKFuxgoIGi13JPfo6hcU49FSF6xiw_RGXbfeXlFHXGy5jRjVT0dWflWzQoVZkTd344wcHVPYA8xn-gjC_Qfwpz_F3Ocj0KhjAcXEQP8E-_j4mmMi-GG2erYx_ILTMykl6jslu12thiHAKB9uNPKVFdIxs-pzpdlbhBblD13Z5o7LUEOtnOcPHTfuOtjQ-GysaOG_wst3Cx5mV3'
-
-class PluginIcon extends Component {
-  render() {
-    return (
-      <SvgIcon>
-        <g fill="none" fillRule="evenodd" transform="translate(0,-1)">
-          <g fill="none" fillRule="evenodd">
-            <g
-              className='spotify-icon'
-              transform="translate(1.000000, 1.000000)"
-            >
-              <g>
-                <path
-                  d="m7.49996,1.06347c-3.55479,0 -6.43665,2.88178 -6.43665,6.43657c0,3.55494 2.88186,6.43649 6.43665,6.43649c3.55517,0 6.43672,-2.88155 6.43672,-6.43649c0,-3.55456 -2.88155,-6.43626 -6.4368,-6.43626l0.00008,-0.00031zm2.9518,9.28338c-0.11529,0.18908 -0.36279,0.24903 -0.55187,0.13297c-1.51126,-0.92311 -3.41374,-1.13218 -5.65427,-0.62028c-0.21591,0.04919 -0.43112,-0.08609 -0.48031,-0.30207c-0.04942,-0.21598 0.08532,-0.4312 0.30176,-0.48039c2.45189,-0.5604 4.55507,-0.31898 6.25172,0.71789c0.18908,0.11606 0.24903,0.36279 0.13297,0.55187zm0.78783,-1.75284c-0.14527,0.23635 -0.45425,0.31091 -0.69022,0.16564c-1.73016,-1.06369 -4.36752,-1.37168 -6.41397,-0.75048c-0.2654,0.08017 -0.54572,-0.06941 -0.62627,-0.33435c-0.07994,-0.2654 0.06971,-0.54518 0.33466,-0.62589c2.3376,-0.70928 5.24367,-0.36571 7.23055,0.85524c0.23597,0.14527 0.31052,0.45425 0.16525,0.68991l0,-0.00008zm0.06764,-1.82501c-2.0745,-1.23217 -5.49716,-1.34547 -7.47782,-0.74433c-0.31805,0.09646 -0.6544,-0.08309 -0.75079,-0.40114c-0.09638,-0.31821 0.08301,-0.65433 0.4013,-0.75102c2.27365,-0.69022 6.05334,-0.55686 8.44174,0.86101c0.28669,0.16979 0.38047,0.53926 0.2106,0.82496c-0.1691,0.28608 -0.53957,0.38039 -0.82473,0.21052l-0.00031,0z"
-                  fill="#1ED760"
-                />
-              </g>
-            </g>
-          </g>
-        </g>
-
-        <style jsx>{`
-          .spotify-icon {
-            fill: #1ED760;
-          }
-        `}</style>
-      </SvgIcon>
-    )
-  }
-}
+const API_TOKEN = 'BQCSXkjsQBPxOQZEegZan2XuC67WhuOMVCswhV0LzBwtJJkNX5WrXlglczURNfkY7VuwzNNqhMyhzsBV6gSssDwgNkXduHrKxGt11gxcQDmroAmrBjZi0-ImGTJvhWGZW3ZvbE73joO8vfQIC40PNQlZXigobRls1a_iZuelMgDaSptsLqa7gi2KzcuyZHxKL-l2t3jh2gV62SoS3ptaS69TO89FyYZim3L-j4ApP_o3H2_8mqDM4pJpIQyMK0r2pDfx3SKpAJwrwPKPod8jf3plhpry2gN3iLiuRb2Hw9q1'
 
 export default class Spotify extends Component {
   static displayName() {
@@ -308,17 +278,27 @@ export default class Spotify extends Component {
     this.seek(Math.floor(position))
   }
 
+  formatTime(ms) {
+    const seconds = Math.floor(ms / 1000)
+    const min = Math.floor(seconds / 60)
+    const sec = seconds % 60
+    return `${min}:${sec.toString().padStart(2, '0')}`
+  }
+
   render() {
     if (!this.state.loaded) return null;
 
-    const { showMiniPlayer, isPlaying, track, album, artist, image, progress, duration, volumePercent, trackSaved, trackId } = this.state
+    const { showMiniPlayer, isPlaying, track, artist, image, progress, duration, volumePercent, trackSaved, trackId } = this.state
     const volumeLines = Math.floor((volumePercent+15)/20)
 
     return (
       <div className='wrapper'
            onMouseDown={(ev) => this.onMouseDown(ev)}
            onWheel={(ev) => this.onWheel(ev)}>
-        <PluginIcon /> {isPlaying ? '▶' : '❚❚'} {artist} — {track}
+        <PluginIcon />
+        <span className="play-status">
+          {isPlaying ? '▶' : '❚❚'} {artist} — {track}
+        </span>
         <div className="volume-level">
           {new Array(5).fill(0).map((_, index) => (
             <div className="volume-line"
@@ -328,14 +308,15 @@ export default class Spotify extends Component {
         {showMiniPlayer ? (
           <div className="popup" onMouseDown={(ev) => ev.stopPropagation()}>
             <div className="album-cover" style={{ backgroundImage: `url(${image})` }} />
-            <div className="controls">
-              <span className="control previous" onClick={this.previous}>⇤</span>
-              {isPlaying ? (
-                <span className="control pause" onClick={this.pause}>❚❚</span>
-              ) : (
-                <span className="control play" onClick={this.play}>▶</span>
-              )}
-              <span className="control next" onClick={this.next}>⇥</span>
+            <div className="top-row">
+              <div className="titles">
+                <div className="titles-track">
+                  <FloatingText text={track} />
+                </div>
+                <div className="titles-artist">
+                  <FloatingText text={artist} />
+                </div>
+              </div>
               {trackSaved ? (
                 <div className="control heart-button save-track" onClick={() => this.deleteTrack(trackId)}>♥</div>
               ) : (
@@ -345,6 +326,23 @@ export default class Spotify extends Component {
             <div className="timeline" ref={(ref) => this.timelineRef = ref} onClick={(ev) => this.onTimelineClick(ev)}>
               <div className="timeline-progress" style={{ width: `${progress/duration*100}%` }} />
             </div>
+            <div className="time">
+              <div className="time-progress">
+                {this.formatTime(progress)}
+              </div>
+              <div className="time-duration">
+                {this.formatTime(duration)}
+              </div>
+            </div>
+            <div className="controls">
+              <span className="control previous" onClick={this.previous}>⇤</span>
+              {isPlaying ? (
+                <span className="control pause" onClick={this.pause}>❚❚</span>
+              ) : (
+                <span className="control play" onClick={this.play}>▶</span>
+              )}
+              <span className="control next" onClick={this.next}>⇥</span>
+            </div>
           </div>
         ) : null}
 
@@ -353,6 +351,10 @@ export default class Spotify extends Component {
             display: flex;
             align-items: center;
             margin-left: auto;
+            color: #1ED760;
+          }
+          .play-status {
+            color: white;
           }
           .popup {
             position: absolute;
@@ -369,14 +371,30 @@ export default class Spotify extends Component {
             margin-bottom: 10px;
             background-size: contain;
           }
+          .top-row {
+            position: relative;
+          }
+          .titles {
+            margin-bottom: 10px;
+            margin-right: 35px;
+            overflow: hidden;
+          }
+          .titles-track {
+            white-space: nowrap;
+          }
+          .titles-artist {
+            width: 100%;
+            font-size: 8px;
+            white-space: nowrap;
+            color: rgba(0, 255, 0, .6);
+          }
           .controls {
             position: relative;
             display: flex;
             justify-content: center;
             align-items: center;
-            margin-bottom: 10px;
+            margin-bottom: 5px;
             font-size: 20px;
-            color: #1ED760;
           }
           .control {
             margin: 0 10px;
@@ -395,11 +413,30 @@ export default class Spotify extends Component {
           .control.pause {
             font-size: 13px;
           }
+          .time {
+            display: flex;
+            justify-content: space-between;
+            font-size: 8px;
+            color: rgba(0, 255, 0, .6);
+          }
           .timeline {
             cursor: pointer;
+            background: rgba(0, 255, 0, .15);
           }
           .timeline-progress {
+            position: relative;
             height: 5px;
+            margin-bottom: 5px;
+            background: #1ED760;
+          }
+          .timeline-progress::before {
+            position: absolute;
+            top: -2px;
+            right: 0;
+            content: '';
+            display: block;
+            width: 2px;
+            height: 8px;
             background: #1ED760;
           }
           .volume-level {
@@ -432,3 +469,116 @@ export default class Spotify extends Component {
     )
   }
 }
+
+class FloatingText extends Component {
+  constructor() {
+    super()
+    this.textRef = null
+    this.wrapperRef = null
+    this.textWidth = null
+    this.wrapperWidth = null
+    this.spacing = 50
+
+    this.onTransitionEnd = this.onTransitionEnd.bind(this)
+  }
+
+  componentDidMount() {
+    if (this.textRef && this.wrapperRef) {
+      this.init()
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.text !== prevProps.text) {
+      this.clearFloating()
+      this.init()
+    }
+  }
+
+  init() {
+    this.textWidth = this.textRef.offsetWidth
+    this.wrapperWidth = this.wrapperRef.offsetWidth
+    this.isFloating = this.textWidth > this.wrapperWidth
+    if (this.isFloating) {
+      this.startFloating()
+    }
+  }
+
+  clearFloating() {
+    this.textRef.style.transitionDuration = '0ms'
+    this.textRef.style.marginLeft = 0
+  }
+
+  startFloating() {
+    this.textRef.style.transitionDuration = `${this.textWidth * 50}ms`
+    this.textRef.style.marginLeft = `-${this.textWidth + this.spacing}px`
+  }
+
+  onTransitionEnd() {
+    this.clearFloating()
+    setTimeout(() => {
+      this.startFloating()
+    }, 2000)
+  }
+
+  render() {
+    const { text, width = '100%' } = this.props
+
+    return (
+      <div>
+        <div className="title-wrapper" ref={(ref) => this.wrapperRef = ref}>
+          <div className="title-1"
+               ref={(ref) => this.textRef = ref}
+               onTransitionEnd={this.onTransitionEnd}>
+            {text}</div>
+          {this.isFloating ? (
+            <div className="title-2">{text}</div>
+          ) : null}
+        </div>
+
+        <style jsx>{`
+          .title-wrapper {
+            display: flex;
+            width: ${width};
+          }
+          .title-1 {
+            margin-right: ${this.spacing}px;
+            transition-property: margin-left;
+            transition-timing-function: linear;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+class PluginIcon extends Component {
+  render() {
+    return (
+      <SvgIcon>
+        <g fill="none" fillRule="evenodd" transform="translate(0,-1)">
+          <g fill="none" fillRule="evenodd">
+            <g
+              className='spotify-icon'
+              transform="translate(1.000000, 1.000000)"
+            >
+              <g>
+                <path
+                  d="m7.49996,1.06347c-3.55479,0 -6.43665,2.88178 -6.43665,6.43657c0,3.55494 2.88186,6.43649 6.43665,6.43649c3.55517,0 6.43672,-2.88155 6.43672,-6.43649c0,-3.55456 -2.88155,-6.43626 -6.4368,-6.43626l0.00008,-0.00031zm2.9518,9.28338c-0.11529,0.18908 -0.36279,0.24903 -0.55187,0.13297c-1.51126,-0.92311 -3.41374,-1.13218 -5.65427,-0.62028c-0.21591,0.04919 -0.43112,-0.08609 -0.48031,-0.30207c-0.04942,-0.21598 0.08532,-0.4312 0.30176,-0.48039c2.45189,-0.5604 4.55507,-0.31898 6.25172,0.71789c0.18908,0.11606 0.24903,0.36279 0.13297,0.55187zm0.78783,-1.75284c-0.14527,0.23635 -0.45425,0.31091 -0.69022,0.16564c-1.73016,-1.06369 -4.36752,-1.37168 -6.41397,-0.75048c-0.2654,0.08017 -0.54572,-0.06941 -0.62627,-0.33435c-0.07994,-0.2654 0.06971,-0.54518 0.33466,-0.62589c2.3376,-0.70928 5.24367,-0.36571 7.23055,0.85524c0.23597,0.14527 0.31052,0.45425 0.16525,0.68991l0,-0.00008zm0.06764,-1.82501c-2.0745,-1.23217 -5.49716,-1.34547 -7.47782,-0.74433c-0.31805,0.09646 -0.6544,-0.08309 -0.75079,-0.40114c-0.09638,-0.31821 0.08301,-0.65433 0.4013,-0.75102c2.27365,-0.69022 6.05334,-0.55686 8.44174,0.86101c0.28669,0.16979 0.38047,0.53926 0.2106,0.82496c-0.1691,0.28608 -0.53957,0.38039 -0.82473,0.21052l-0.00031,0z"
+                  fill="#1ED760"
+                />
+              </g>
+            </g>
+          </g>
+        </g>
+
+        <style jsx>{`
+          .spotify-icon {
+            fill: #1ED760;
+          }
+        `}</style>
+      </SvgIcon>
+    )
+  }
+}
+
